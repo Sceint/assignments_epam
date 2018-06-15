a620c5381f5be49af85724629097b4a659faeb16
KITHYD\KIT9070 <arpan.ladani@kony.com>
2018-06-16 02:11:40 +0530

    Code added for Quick Sort and Test cases

--- week1/quick_sort/src/main/java/com/week1/QuickSort.java
+++ week1/quick_sort/src/main/java/com/week1/QuickSort.java
@@ -4,8 +4,52 @@ import java.util.Arrays;
 
 public class QuickSort {
 
-public String sortbyQuicksort(String input) {
+	public String sortbyQuicksort(String input) {
 		
-		return "";
+		int arr[];
+		StringBuilder result = new StringBuilder();
+		try {
+			arr = Arrays.stream(input.split(" ")).mapToInt(Integer::parseInt).toArray();
+			
+			arr = quickSortAlgo(arr, 0, arr.length - 1);
+			
+			for(int i: arr)
+				result.append(i + " ");
+		}
+		catch(NumberFormatException e) {
+			System.err.print("Error in Conversion : ");
+			e.printStackTrace();
+		}
+		
+		return result.toString().trim();
+	}
+	
+	private int[] quickSortAlgo(int[] arrNew, int low, int high) {
+		int i = low;
+		int j = high;
+		int p = arrNew[low + (high - low) / 2];
+		
+		while(i <= j) {
+			while(arrNew[i] < p)
+					i++;
+			
+			while(arrNew[j] > p)
+				j--;
+			
+			if(i <= j) {
+				int temp = arrNew[i];
+				arrNew[i] = arrNew[j];
+				arrNew[j] = temp;
+				i++;
+				j--;
+			}
+		}
+		
+		if (low < j)
+			quickSortAlgo(arrNew, low, j);
+        if (i < high)
+        	quickSortAlgo(arrNew, i, high);
+		
+		return arrNew;
 	}
 }
--- week1/quick_sort/src/test/java/com/week1/QuickSortTest.java
+++ week1/quick_sort/src/test/java/com/week1/QuickSortTest.java
@@ -8,7 +8,15 @@ public class QuickSortTest {
 
 	@Test
 	public void test() {
+		QuickSort t = new QuickSort();
+		String result = t.sortbyQuicksort("45 3 23 12 54");
+		assertEquals("3 12 23 45 54", result);
 		
+		result = t.sortbyQuicksort("54 32 12 564 12 34 54 65 1");
+		assertEquals("1 12 12 32 34 54 54 65 564", result);
+		
+		result = t.sortbyQuicksort("76 32 90 12 4252 -12 0 23");
+		assertEquals("-12 0 12 23 32 76 90 4252", result);
 	}
 
 }
