d2c6ea7af07040261f5bf263aa2e06b4ca81df3e
KITHYD\KIT9070 <arpan.ladani@kony.com>
2018-06-16 01:55:29 +0530

    Initial quick_sort project commit

---
+++ week1/quick_sort/src/main/java/com/week1/QuickSort.java
@@ -0,0 +1,11 @@
+package com.week1;
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+public String sortbyQuicksort(String input) {
+		
+		return "";
+	}
+}
---
+++ week1/quick_sort/src/test/java/com/week1/QuickSortTest.java
@@ -0,0 +1,14 @@
+package com.week1;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class QuickSortTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
