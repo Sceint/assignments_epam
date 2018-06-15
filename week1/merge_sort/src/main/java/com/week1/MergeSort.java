492419681e7956353bde394be2ccfd57525d6210
KITHYD\KIT9070 <arpan.ladani@kony.com>
2018-06-15 23:34:16 +0530

    Intial merge_sort project commit

---
+++ week1/merge_sort/src/main/java/com/week1/MergeSort.java
@@ -0,0 +1,11 @@
+package com.week1;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+public String sortbyMergesort(String input) {
+
+	return "";
+	}
+}
---
+++ week1/merge_sort/src/test/java/com/week1/MergeSortTest.java
@@ -0,0 +1,14 @@
+package com.week1;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class MergeSortTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
