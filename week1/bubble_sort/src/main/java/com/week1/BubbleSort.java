66ec09dc8427dacd266dc85398749b36678f93e7
KITHYD\KIT9070 <arpan.ladani@kony.com>
2018-06-15 23:24:18 +0530

    BubbleSort algorithm and test cases added

--- week1/bubble_sort/src/main/java/com/week1/BubbleSort.java
+++ week1/bubble_sort/src/main/java/com/week1/BubbleSort.java
@@ -1,9 +1,34 @@
 package com.week1;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class BubbleSort {
 	
 	public String sortbyBubblesort(String input) {
 		
-		return "";
+		StringBuilder result = new StringBuilder();
+		try {
+			int[] arr = Arrays.stream(input.split(" ")).mapToInt(Integer::parseInt).toArray();
+			
+			for(int i = 0; i < arr.length - 1; i++) {
+				for(int j = i + 1; j < arr.length; j++) {
+					if(arr[i] > arr[j]) {
+						int temp = arr[i];
+						arr[i] = arr[j];
+						arr[j] = temp;
+					}
+				}
+			}
+			
+			for(int i: arr)
+				result.append(i + " ");
+		}
+		catch(NumberFormatException e) {
+			System.err.print("Error in Conversion : ");
+			e.printStackTrace();
+		}
+		
+		return result.toString().trim();
 	}
 }
